72a22f36cae56adfd8daf698eac9e82bff1ec98f
Sanusi Salihu <sanuseesalihu2020@gmail.com>
2024-05-31 16:52:47 +0100

    Create the product page, handle the routing in App.js, create productsData array in data folder and use it in the products page

---
+++ src/data/productsData.jsx
@@ -0,0 +1,28 @@
+import { wreen_actionname, wreen_busylady, wreen_coffee } from "../assets/images"
+
+export const productsData = [
+    {
+        id: "874320",
+        productname: "Pro Boost Shoe",
+        price:44356,
+        image: wreen_actionname,
+        inventories:0,
+        status:  "Out Of Stock",
+    },
+    {
+        id: "874321",
+        productname: "Pro Shoe",
+        price:74356,
+        image: wreen_busylady,
+        inventories:30,
+        status:  "Available",
+    },
+    {
+        id: "874322",
+        productname: "Pro",
+        price:37337,
+        image: wreen_coffee,
+        inventories:25,
+        status:  "Available",
+    },
+]
